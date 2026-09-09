4c66af61298db2744624f352b326ae1595bfdc47
Mariano18014 <hasanmariano5@gmail.com>
2026-09-09 08:30:38 -0300

    feat: add central IndicatorValueService with idempotent saveOrUpdate and race handling [HU-04]

--- src/main/java/com/indicadoresar/common/config/RestClientConfig.java
+++ src/main/java/com/indicadoresar/common/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.indicadoresar.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,11 @@ import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 
     @Bean
     public RestClient.Builder restClientBuilder() {
